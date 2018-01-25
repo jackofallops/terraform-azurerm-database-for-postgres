7d04f8373d91dd67eba35f1a88d6a84f0e640f50
sjones-sot <steve@sourceoftruth.co.uk>
2018-01-25 08:46:33 +0000

    Put index count on the correct interpolated attribute

--- modules/firewall-rule-list/rule-list.tf
+++ modules/firewall-rule-list/rule-list.tf
@@ -1,6 +1,6 @@
 resource "azurerm_postgresql_firewall_rule" "postgres_firewall_rule_list" {
-  name                = "${var.rule_name}"
-  resource_group_name = "${var.resource_group_name}-${count.index}"
+  name                = "${var.rule_name}-${count.index}"
+  resource_group_name = "${var.resource_group_name}"
   server_name         = "${var.postgres_server_name}"
   start_ip_address    = "${element(var.ip_address_list, count.index)}"
   end_ip_address      = "${element(var.ip_address_list, count.index)}"
